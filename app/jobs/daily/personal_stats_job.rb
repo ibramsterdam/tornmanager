928810bea3b46aa6787d7547864c497fa389f878
Bram <bramjanssen@hey.com>
2026-01-21 21:40:58 +0100

    update personalstats job

--- app/jobs/daily/personal_stats_job.rb
+++ app/jobs/daily/personal_stats_job.rb
@@ -3,14 +3,11 @@ module Daily
     queue_as :default
 
     def perform(*args)
-      batch_size = 50
-      delay_between_batches = 1.minute
+      batch_size = 60
 
-      TornUser.find_in_batches(batch_size: batch_size).with_index do |users, batch_index|
-        run_time = Time.current + batch_index * delay_between_batches
-
+      TornUser.find_in_batches(batch_size: batch_size).with_index do |users, i|
         users.each do |torn_user|
-          FetchPersonalStatsJob.set(wait_until: run_time).perform_later(torn_user)
+          FetchPersonalStatsJob.set(wait_until: i.minutes).perform_later(torn_user)
         end
       end
     end
