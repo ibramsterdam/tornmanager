f0f95a54b9794e8df01acb7122c60aafdcf5f1e8
Bram <bramjanssen@hey.com>
2026-01-21 20:17:09 +0100

    changes

--- app/jobs/daily/factionhof_members_job.rb
+++ app/jobs/daily/factionhof_members_job.rb
@@ -2,17 +2,17 @@ class Daily::FactionhofMembersJob < ApplicationJob
   queue_as :default
 
   FACTION_BATCH_SIZE = 50
-  TOP_FACTIONS_COUNT = 2000
+  TOP_FACTIONS_COUNT = 4000
 
   def perform
     api_key = Rails.application.credentials.dig(:bram, :api_key)
-    # 10 calls
+    # 20 calls
     top_factions = (0...TOP_FACTIONS_COUNT).step(100).flat_map do |offset|
-      TornApi::Torn::Factionhof.new(api_key, offset).fetch
+      TornApi::Torn::Factionhof.new(api_key, offset:).fetch
     end
     faction_ids = top_factions.map(&:torn_id)
 
-    # enqueue 50 api calls 20 times, so spread over 20 minutes
+    # enqueue 50 api calls 40 times, so spread over 20 minutes
     faction_ids.each_slice(FACTION_BATCH_SIZE).with_index do |batch_ids, i|
       FactionMembersJob.set(wait: i.minutes).perform_later(batch_ids)
     end
--- app/jobs/fetch_personal_stats_job.rb
+++ app/jobs/fetch_personal_stats_job.rb
@@ -1,14 +1,17 @@
 class FetchPersonalStatsJob < ApplicationJob
   queue_as :default
 
+  MIN_STAT_ENHANCER = 200
+
   def perform(torn_user)
     stats = TornApi::User::PersonalStats.new(api_key, torn_user.torn_id).fetch
+    torn_user.update!(hof_stats_user: true) if stats.items_used_stat_enhancers > MIN_STAT_ENHANCER
     torn_user.personal_stat_snapshots.create!(stats.to_h)
   end
 
   private
 
   def api_key
-    api_key ||=Rails.application.credentials.dig(:bram, :api_key)
+    Rails.application.credentials.dig(:bram, :api_key)
   end
 end
