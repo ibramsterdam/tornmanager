c4d7fc6b0e1727e490b5fccf17621a3e6c71d393
Bram <bramjanssen@hey.com>
2026-01-26 20:23:17 +0100

    harvest hof_users

--- app/jobs/daily/personal_stats_job.rb
+++ app/jobs/daily/personal_stats_job.rb
@@ -2,13 +2,13 @@ module Daily
   class PersonalStatsJob < ApplicationJob
     queue_as :default
 
-    def perform(delay = 1)
+    def perform(delay = 15)
       batch_size = 60
       run_at = delay.minutes.from_now
 
       TornUser.hof_stats_users.find_in_batches(batch_size:) do |users|
         users.each do |torn_user|
-          FetchPersonalStatsJob.set(wait_until: delay_time.from_now).perform_later(torn_user)
+          FetchPersonalStatsJob.set(wait_until: run_at).perform_later(torn_user)
           run_at += 1.second
         end
       end
