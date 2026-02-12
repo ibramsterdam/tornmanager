8207ebc69bf43c22aca860a589f786f0628a044c
Bram <ibramsterdam@hotmail.com>
2026-02-12 21:48:50 +0100

    allow authentication

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   allow_unauthenticated_access
   def index
+    redirect_to progress_index_path if authenticated?
   end
 end
--- app/models/torn_api/user/profile.rb
+++ app/models/torn_api/user/profile.rb
@@ -3,13 +3,33 @@ module TornApi
     class Profile < Base
       ENDPOINT = "v2/user/basic".freeze
 
+      ProfileData = Data.define(
+        :id,
+        :name,
+        :level,
+        :gender,
+        :status
+      )
+
       def fetch
         response = get(ENDPOINT, striptags: false)
         if response["profile"].present?
-          response["profile"]
+          parse_profile(response["profile"])
         else
           raise InvalidKeyError, "Torn API authentication failed: #{response}"
         end
+      end
+
+      private
+
+      def parse_profile(data)
+        ProfileData.new(
+          id: data["id"],
+          name: data["name"],
+          level: data["level"],
+          gender: data["gender"],
+          status: data["status"]
+        )
       end
     end
   end
