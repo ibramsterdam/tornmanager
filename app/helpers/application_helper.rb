610bc2a814b24be349193819c73137eb1bee4139
Bram <bramjanssen@hey.com>
2026-02-23 20:55:14 +0100

    keep navbar faction links contextual to the faction being viewed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  # Returns the faction currently being viewed (from URL), or falls back to the user's own faction.
+  # Used by the navbar to keep faction links contextual to the page being viewed.
+  def navbar_faction
+    @_navbar_faction ||= begin
+      torn_id = params[:faction_torn_id] || params[:torn_id]
+      if torn_id.present?
+        Faction.find_by(torn_id: torn_id)
+      end
+    end || Current.user&.faction
+  end
+
+  def viewing_other_faction?
+    navbar_faction.present? &&
+      Current.user&.faction.present? &&
+      navbar_faction != Current.user.faction
+  end
 end
